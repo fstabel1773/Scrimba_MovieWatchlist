70dd02e3353892fd7d767f10113fb3e30b512db4
Florian Stabel <f.stabel@gmx.de>
2023-02-01 09:59:14 +0100

    WL03 - feat: read-more-btn and read-less-btn

--- index.js
+++ index.js
@@ -40,6 +40,18 @@ document.addEventListener("click", async (event) => {
     renderMovies();
   }
   if (event.target.id === "read-more-btn") {
+    let plotText = event.target.closest(".plot-info").textContent;
+    event.target.closest(".plot-container").innerHTML = getPlotHtml(
+      plotText,
+      true
+    );
+  }
+  if (event.target.id === "read-less-btn") {
+    let plotText = event.target.closest(".plot-info").textContent;
+    event.target.closest(".plot-container").innerHTML = getPlotHtml(
+      plotText,
+      false
+    );
   }
 });
 
@@ -77,7 +89,7 @@ async function getMovieData() {
   let promisedDetailedMovieDataArray = basicMovieDataArray.map(
     async (basicMovieObject) => {
       const detailedMovieObject = await fetch(
-        `http://www.omdbapi.com/?apikey=fa0d068f&i=${basicMovieObject.imdbID}&plot=short`
+        `http://www.omdbapi.com/?apikey=fa0d068f&i=${basicMovieObject.imdbID}&plot=full`
       );
       const detailedMovieData = await detailedMovieObject.json();
       return detailedMovieData;
@@ -100,6 +112,7 @@ async function getMoviesHtml() {
 
   movieArray.forEach((movieObject) => {
     const watchlistBtn = getWatchlistBtnHtml(movieObject);
+    const plotTeaserHtml = getPlotHtml(movieObject.Plot, false, 150);
 
     moviesHtml += `
       <div class="movie-wrapper">
@@ -117,16 +130,16 @@ async function getMoviesHtml() {
               </button>
             </div>
             <div class="movie-info-line-three">
-              <span>${movieObject.Plot}</span>
+            <div class="plot-container">
+              ${plotTeaserHtml}
+            </div>
             </div>
           </div>
       </div>
       <div class="divider"></div>
   `;
   });
-  moviesHtml += `
- 
-    <button id="view-more-btn">View more</button>`;
+  moviesHtml += `<button id="view-more-btn">View more</button>`;
   return moviesHtml;
 }
 
@@ -145,6 +158,31 @@ function getWatchlistBtnHtml(movieObject) {
         <span>Remove</span>
       </button>
       `;
+  }
+}
+
+function getPlotHtml(fullPlotString, fullLength = false, maxLength = 155) {
+  let visiblePlotString = fullPlotString;
+  let invisiblePlotString = ``;
+
+  if (fullPlotString.length <= maxLength) {
+    return `
+      <p class="plot-info">${visiblePlotString}</p>
+    `;
+  } else {
+    if (!fullLength) {
+      visiblePlotString = fullPlotString.slice(0, maxLength + 1);
+      invisiblePlotString = fullPlotString.slice(maxLength + 1);
+      const readMoreBtn = `<input id="read-more-btn" type="button" value="... Read more"</input>`;
+
+      return `
+        <p class="plot-info">${visiblePlotString}${readMoreBtn}<span class="invisible">${invisiblePlotString}<span></p>
+      `;
+    } else {
+      const readLessBtn = `<input id="read-less-btn" type="button" value="Read less"</input>`;
+
+      return `<p class="plot-info">${visiblePlotString}${readLessBtn}</p>`;
+    }
   }
 }
 
@@ -168,3 +206,38 @@ function getWatchlistData() {
 }
 
 renderMovies();
+
+// helper function for splitting plot-info, enabling read-more
+// function splitPlotInfo(string) {
+//   const wordArray = string.split(" ");
+//   let lineOne = "";
+//   let lineTwo = "";
+//   let lineMore = "";
+//   wordArray.forEach((word, index) => {
+//     if (index < 6) {
+//       lineOne += `${word} `;
+//     } else if (index < 12) {
+//       lineTwo += `${word} `;
+//     } else if (index >= 12 && index < wordArray.length - 1) {
+//       lineMore += `${word} `;
+//     } else {
+//       lineMore += `${word}`;
+//     }
+//   });
+
+//   return [lineOne, lineTwo, lineMore];
+// }
+
+// // overflow doesn't exist if height of movie-wrapper is set to "auto" -> toggle via read-more-btn
+// function checkForOverflow() {
+//   const movieWrapper = document.querySelectorAll("movie-wrapper");
+//   movieWrapper.forEach((movieHtml) => {
+//     if (movieHtml.offsetHeight < movieHtml.scrollHeight) {
+//       let bla = $(document).ready(() => {
+//         $("div").find(".plot-info");
+//       });
+//       console.log("test");
+//       console.log(bla);
+//     }
+//   });
+// }
